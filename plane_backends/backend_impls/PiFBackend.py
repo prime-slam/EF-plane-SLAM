ab2f1add5e3b2d537c10f52405bb1ab8d003e238
Dmitriy Jarosh <dimajarosh@gmail.com>
2023-03-19 21:29:25 +0300

    Fix License

--- plane_backends/backend_impls/PiFBackend.py
+++ plane_backends/backend_impls/PiFBackend.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, Dmitrii Iarosh, Anastasiia Kornilova
+# Copyright (c) 2022, Gonzalo Ferrer, Dmitrii Iarosh, Anastasiia Kornilova
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
